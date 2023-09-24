8dc64969a525c4c84ffcaa8a6a4ec813f5f3c7a0
Sumiya Chowdhury <sumiyachowdury184@gmail.com>
2023-09-24 12:10:07 +0300

    Home page Final

--- ngo-website/src/components/Header.js
+++ ngo-website/src/components/Header.js
@@ -7,8 +7,7 @@ function Header() {
   return (
     <div className="header">
       <div className="logo">
-        {/* <img src={process.env.PUBLIC_URL + '/images/logo.PNG'} alt="Nisum Logo" /> */}
-        <img src="/images/logo.PNG" alt='Nisum logo'/>
+        <img src={process.env.PUBLIC_URL + '/logoo.PNG'} alt="Nisum Logo" />
       </div>
       <div className="ngo-name">Nisum</div>
       <div className="header-links">
@@ -19,6 +18,7 @@ function Header() {
       </div>
       <button className="donate-button">DONATE</button>
       <input type="text" placeholder="Search" className="search-bar" />
+
     </div>
   );
 }
